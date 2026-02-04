f72763b2d5002e2f43655c86c762c6035198354b
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-04 16:32:16 +0000

    Lots of inequalities

--- Verifier.cs
+++ Verifier.cs
@@ -2,4 +2,47 @@
 public class Verifier
 {
     // Add your code here
+    public string OldEnough(int age)
+    {
+        if(age > 17)
+        {
+            return "old enough";
+        } else
+        {
+            return "";
+        }
+    }
+
+    public string UnderAge(int age)
+    {
+        if(age < 18)
+        {
+            return "underage";
+        } else
+        {
+            return "";
+        }
+    }
+
+    public string LegalSpeed(int speed)
+    {
+        if(speed <= 50)
+        {
+            return "legal speed";
+        }  else
+        {
+            return "";
+        }
+    }
+
+    public string IsGoldenYears(int age)
+    {
+        if(age >= 65)
+        {
+            return "golden years";
+        }  else
+        {
+            return "";
+        }
+    }
 }
